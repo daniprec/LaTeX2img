c64a7bc88c706ff7166927e7682c56f04f380a6c
daniprec <daniel.precioso@uca.es>
2024-06-05 10:12:41 +0200

    Get choices right

--- main.py
+++ main.py
@@ -4,33 +4,29 @@ from typing import List
 import matplotlib.pyplot as plt
 
 
-def plot_expression(latex_expression: str):
-    """
-    Source: https://medium.com/@ealbanez/how-to-easily-convert-latex-to-images-with-python-9062184dc815
-    """
-    # Ensure the expression is an r-string
-    latex_expression = r"$" + latex_expression + r"$"
-    text = plt.text(
-        x=0.5,  # x-coordinate to place the text
-        y=0.5,  # y-coordinate to place the text
-        s=latex_expression,
-        horizontalalignment="center",
-        verticalalignment="center",
-        fontsize=16,
-    )
-    return text
+def tex_to_img(tex: str, fout: str):
+    plt.figure(figsize=(8, 0.3))
+    plt.text(-0.1, 0.0, tex, ha="left", va="bottom", fontsize=12)
+    plt.axis("off")
+    plt.savefig(fout)
+    plt.close()
 
 
 def process_question(lines: List[str], fout: str):
-    text = " ".join(lines)
-    # Caught the equations encapsulated by "$" or "\[\]" or "\begin{equation}"
-    equations = [
-        eq.strip() for eq in text.split("$")[1:] if eq.strip() and eq.strip()[0] != "\\"
-    ]
-    for idx, eq in enumerate(equations):
-        plot_expression(eq)
-        plt.savefig(fout + f"_equation_{idx:03d}.png")
-        plt.close()
+    idx_choice = 0
+    for line in lines:
+        if line.strip().startswith("\\choice"):
+            idx_choice += 1
+            if "\\choice[!]" in line:
+                extra = "_correct"
+            else:
+                extra = ""
+            # Extract the text inside "{}"
+            txt = line[line.find("{") + 1 : line.rfind("}")]
+            try:
+                tex_to_img(txt, fout + f"_A{idx_choice}{extra}.png")
+            except Exception as e:
+                print(repr(e))
 
 
 def read_tex(path_file: str, path_output: str):
@@ -49,8 +45,8 @@ def read_tex(path_file: str, path_output: str):
             elif line.strip() == "\\end{question}":
                 end_index = i
             if start_index is not None and end_index is not None:
-                fout = path_output + f"/question_{question_index:03d}"
-                process_question(lines[start_index : end_index + 1], fout)
+                fout = path_output + f"/Q{question_index:03d}"
+                process_question(lines[start_index + 1 : end_index], fout)
                 start_index = None
                 end_index = None
 
